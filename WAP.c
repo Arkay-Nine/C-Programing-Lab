3abe12b66833283f10870286e924c5a4d72e9f61
Arkay <ayankhan09092006@gmail.com>
2025-10-13 09:27:39 +0530

    Update WAP.c

--- WAP.c
+++ WAP.c
@@ -1,1 +1,30 @@
-#include
+
+UIN 251M031
+
+NAME: MOHAMMAD AYAN KHAN
+
+#include <stdio.h>
+
+int main() {
+
+int initial;
+
+int final:
+
+int add = 0;
+
+printf("\nEnter initial Number :"); scanf("%d",&initial); printf("\nEnter final Number :"); scanf("%d", &final);
+
+for(int i = initial; i<= final; i++)
+
+if(i%2 != 0)
+
+add = add+i:
+
+printf("\nSum of odd Number between %d",initial);
+
+printf(" and %d", final);
+
+printf(" is => %d", add);
+
+return 0;
